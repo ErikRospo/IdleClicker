d135f1fd971cea0fccac0f09fc509e77d004ed4b
ErikRospo <rerik.work@gmail.com>
2022-01-04 14:29:08 -0800

    Updated draw_canvas_script.js with helper functions

--- js/draw_canvas_script.js
+++ js/draw_canvas_script.js
@@ -1,2 +1,18 @@
 var canv = document.getElementById('DrawCanvas');
 var ctx = canv.getContext("2d");
+window.onLoad({
+  startup(ctx);
+});
+function startup(ctx) {
+
+};
+function line(ctx,x1,y1,x2,y2) {
+  ctx.moveTo(x1,y1);
+  ctx.lineTo(x2,y2);
+  ctx.stroke();
+};
+function circle(ctx,x1,y1,r) {
+    ctx.beginPath();
+    ctx.arc(x1,y1,r,0,2*Math.PI);
+    ctx.stroke();
+}
