0aab6bd511ffdefc1440b4e49307bcac2b6a290d
ErikRospo <rerik.work@gmail.com>
2022-01-04 15:52:13 -0800

    Added better css, as well as the start to an image drawing function

--- js/draw_canvas_script.js
+++ js/draw_canvas_script.js
@@ -1,26 +1,29 @@
 var canv = document.getElementById('DrawCanvas');
 var ctx = canv.getContext("2d");
 window.onload({
-  startup(ctx);
+  startup();
 });
-function startup(ctx) {
+function startup() {
 
 };
-function line(ctx,x1,y1,x2,y2) {
+function line(x1,y1,x2,y2) {
   ctx.moveTo(x1,y1);
   ctx.lineTo(x2,y2);
   ctx.stroke();
 };
-function circle(ctx,x1,y1,r) {
+function circle(x1,y1,r) {
     ctx.beginPath();
     ctx.arc(x1,y1,r,0,2*Math.PI);
     ctx.stroke();
 };
-function drawText(ctx,x1,y1,text,font="Arial",fontSize="12pt") {
+function drawText(x1,y1,text,font="Arial",fontSize="12pt") {
   ctx.font=fontSize+" "+font;
   ctx.fillText(text,x1,y1);
 };
-function strokeText(ctx,x1,y1,text,font="Arial",fontSize="12pt") {
+function strokeText(x1,y1,text,font="Arial",fontSize="12pt") {
   ctx.font=fontSize+" "+font;
   ctx.strokeText(text,x1,y1);
 };
+function ImgDraw(img,x,y) {
+  
+}
